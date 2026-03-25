8cf295630f2eb24c5b93c8fbfb6e37be246e9481
Prachi <prachigaydhani220@gmail.com>
2026-03-25 12:26:38 +0530

    Switch Gmail to port 465

--- server.js
+++ server.js
@@ -50,18 +50,12 @@ db.connect((err) => {
 const transporter = nodemailer.createTransport({
 
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-
-  requireTLS: true,
-
-  tls: {
-    rejectUnauthorized: false
   }
 
 });
